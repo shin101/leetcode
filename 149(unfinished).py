a3abd63d2664fafcb45253fb815d5453ddb5ec4d
shin101 <kendras0127@gmail.com>
2023-10-04 17:56:27 -0400

    55 submitted

---
+++ 149(unfinished).py
@@ -0,0 +1,25 @@
+from typing import List
+import collections
+
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+
+# hint
+# hashmap
+# 1. for each pt determine if it lies on the longest slope
+# 2. count all pts with same slope
+# 3. update result with max
+
+class Solution:
+    pass 
+    # def maxPoints(self, points: List[List[int]]) -> int:
+    #     res = 1
+    #     for i in range(len(n)):
+    #         p1 = points[i]
+    #         count = collections.defaultdict(int)
+    #         for j in range(i+1, len(n)):
+    #             p2 = points[j]
+    #             if p2[0] == 
+
+
+        
---
+++ 5.py
@@ -0,0 +1,7 @@
+
+# Given a string s, return the longest 
+# palindromic bsubstringin s.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
