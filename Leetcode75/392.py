1ca748b4551f0a65f4e6b18c98e51641f3fd673f
shin101 <kendras0127@gmail.com>
2023-10-16 14:24:41 -0400

    392

---
+++ Leetcode75/392.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        idx = 0
+        spointer = 0
+        tpointer = 0
+                    
+        while spointer < len(s) and tpointer < len(t):
+
+            if s[spointer]==t[tpointer]:
+                spointer+=1
+            tpointer+=1
+
+        return spointer==len(s)
