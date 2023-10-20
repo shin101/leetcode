4cfa7b4d37739f5879d22d4e3877c34b9f7a3bd2
shin101 <kendras0127@gmail.com>
2023-10-20 16:54:44 -0400

    435

---
+++ Leetcode75/435.py
@@ -0,0 +1,13 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        count = 0
+        prevEnd = intervals[0][1]
+
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                count+=1
+                prevEnd = min(prevEnd, end)
+        return count
