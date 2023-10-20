1635f8046435e320d476fa626c2e71b01eee1fca
shin101 <kendras0127@gmail.com>
2023-10-19 21:50:00 -0400

    1207

---
+++ array/1207.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hashmap = defaultdict(int)
+        for num in arr:
+            hashmap[num] += 1
+        return len(hashmap.values()) == len(set(hashmap.values()))
+        
