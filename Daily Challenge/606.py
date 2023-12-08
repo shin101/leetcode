8837b946cd7b205c086e43a06c18fc0ff6e1ae4e
shin101 <kendras0127@gmail.com>
2023-12-08 12:41:39 -0500

    606

---
+++ Daily Challenge/606.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        res = []
+
+        def preorder(node):
+            if not node:
+                return
+            res.append("(")
+            res.append(str(node.val))
+            preorder(node.left)
+            if not node.left and node.right:
+                res.append("()")
+            preorder(node.right)
+            res.append(")")
+        preorder(root)
+        return "".join(res[1:-1])
