5703d755458f4cb63bc7e3d6e678a0af64d57a64
shin101 <kendras0127@gmail.com>
2023-12-21 09:59:12 -0500

    1637

---
+++ Daily Challenge/1637.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        res = 0
+        points.sort()
+        for i in range(len(points)-1):
+            res = max(res, points[i+1][0] - points[i][0])
+        return res
