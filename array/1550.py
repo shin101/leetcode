a91aa673aede1476b04b64bf80fef82c4f4bf19a
shin101 <kendras0127@gmail.com>
2024-06-30 17:06:16 -0700

    1550

---
+++ array/1550.py
@@ -0,0 +1,14 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0
+
+        for num in arr:
+            if num%2!=0:
+                cnt+=1
+                if cnt == 3:
+                    return True
+            else:
+                cnt = 0
+
+
+        return False
