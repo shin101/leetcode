143f7072182d47ef2c9b76d6f1c543cebaf0d946
shin101 <kendras0127@gmail.com>
2024-08-21 16:59:58 -0700

    2798

---
+++ array/2798.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        res = 0
+        for h in hours:
+            if h>=target:
+                res +=1
+        
+        return res
