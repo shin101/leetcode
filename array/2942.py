4199d0ddec241d2eb79d5ff92a745e1a58d57944
kendra <44033225+shin101@users.noreply.github.com>
2025-05-24 14:15:49 -0700

    Create 2942.py

---
+++ array/2942.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res = []
+
+        for idx, word in enumerate(words):
+            if x in word:
+                res.append(idx)
+                continue
+
+        return res
